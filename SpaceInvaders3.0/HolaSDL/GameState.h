1f8a669c6360c788b317e49765d89156d1d40174
Paulalemany <palemany@ucm.es>
2023-12-12 17:54:11 +0100

    Game State beta

--- SpaceInvaders3.0/HolaSDL/GameState.h
+++ SpaceInvaders3.0/HolaSDL/GameState.h
@@ -9,21 +9,27 @@ using namespace std;
 class GameState
 {
 private:
-	Game* game;
-	GameList<GameObject, true> gameList;
-	list<EventHandler*> lista;
+	Game* game;									//Puntero al juego
+	GameList<GameObject, true> gameList;		//Lista de objetos del juego
+	list<EventHandler*> lista;					//Manejadores de eventos
 
 public:
-	//Hacemos una clase abstracta base para todos los estados del juego
+
 	//Funciones necesarias para todas las clases hijas de GameState
-	virtual void update() = 0;
-	virtual void render() = 0;
+	virtual void Update() = 0;
+	virtual void Render() = 0;
+	virtual void HandleEvent() = 0;
+
 
 	//Funciones de transición entre estados
-	virtual bool onEnter() = 0;
-	virtual bool onExit() = 0;
+	virtual bool OnEnter() = 0;
+	virtual bool OnExit() = 0;
+
+	//Metodos para añadir 
+	virtual void AddEventListener() = 0;
+	virtual void AddObject() = 0;
 
 	//Getter del ID del estado en el que esté
-	virtual std::string getStateID() const = 0;
+	virtual std::string GetStateID() const = 0;
 };
 
